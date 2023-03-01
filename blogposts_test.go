cccf69d2ac3d155068b78ba2c9526e49c121ec96
Frank Jahn <frank.jahn@cgm.com>
2023-03-01 15:50:50 +0000

    extracting description from file

--- blogposts_test.go
+++ blogposts_test.go
@@ -8,41 +8,49 @@ import (
 	"testing/fstest"
 )
 
-func TestBlogPosts(t *testing.T) {
-  t.Run("happy path", func(t *testing.T) { 
-    // Given
-    fs := fstest.MapFS{
-      "hello-world.md": {Data: []byte("Title: hello, world!")},
-      "hello-world2.md": {Data: []byte("Title: Hola, mundo!")},
-    }
-    
-    // When
-    posts, err := blogposts.PostFromFS(fs)
-    
-    // Then
-    if err != nil {
-      t.Fatal(err)
-    }
-    
-    if len(posts)!= len(fs) {
-      t.Errorf("Expected %d posts, got %d", len(fs), len(posts))
-  }
-  
-  want := blogposts.Post{Title:"hello, world!"}
+func TestPostsFromFS(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		// Given
+		fileSystem := fstest.MapFS{
+			"hello-world.md":  {Data: []byte("Title: hello, world!\nDescription: What's up, world!")},
+			"hello-world2.md": {Data: []byte("Title: Hola, mundo!\nDescription: que pasa, mundo!")},
+		}
 
-  if posts[0] != want {
-    t.Errorf("Expected %#v, got %#v", want, posts[0])
-  }
-})
-t.Run("failing fs", func(t *testing.T) {
-  _, err := blogposts.PostFromFS(FailingFS{})
-  if err == nil {
-    t.Error("Expected an error, but didn't get one")
-  }
-})
+		// When
+		posts, err := blogposts.PostFromFS(fileSystem)
+
+		// Then
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(posts) != len(fileSystem) {
+			t.Errorf("Expected %d posts, got %d", len(fileSystem), len(posts))
+		}
+
+		want := blogposts.Post{
+			Title:       "hello, world!",
+			Description: "What's up, world!",
+		}
+
+		assertPost(t, posts[0], want)
+	})
+	t.Run("failing fs", func(t *testing.T) {
+		_, err := blogposts.PostFromFS(FailingFS{})
+		if err == nil {
+			t.Error("Expected an error, but didn't get one")
+		}
+	})
 }
-type FailingFS struct {
+
+func assertPost(t *testing.T, got, want blogposts.Post) {
+	if got != want {
+		t.Errorf("Expected %#v, got %#v", want, got)
+	}
 }
+
+type FailingFS struct {}
+
 func (f FailingFS) Open(name string) (fs.File, error) {
 	var err = errors.New("failed to open")
 	return nil, err
--- post.go
+++ post.go
@@ -1,18 +1,35 @@
 package blogposts
 
 import (
+	"bufio"
 	"io"
 	"strings"
 )
 
 type Post struct {
   Title string
+	Description string
 }
 
 func newPost(blogFile io.Reader) Post {
-	fileContent, _ := io.ReadAll(blogFile)
-	title := strings.TrimPrefix(string(fileContent), "Title: ")
-	return Post{
-		Title: title,
+	// fileContent, _ := io.ReadAll(blogFile)
+	// title := strings.TrimPrefix(string(fileContent), "Title: ")
+	// return Post{
+	// 	Title: title,
+	// }
+	scanner := bufio.NewScanner(blogFile)
+
+	readLine := func(prefix string) string {
+		scanner.Scan() 
+		return strings.TrimPrefix(scanner.Text(), prefix)
+  }
+
+	title := readLine("Title: ")
+	description := readLine("Description: ")
+
+	post := Post{
+		Title:       title,
+		Description: description,
 	}
+	return post
 }
