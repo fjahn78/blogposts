d17cd77ee0fcac21ec562b6b984e51f4e9728faa
Frank Jahn <frank.jahn@cgm.com>
2023-03-01 15:34:56 +0000

    handle read dir errors

--- blogposts.go
+++ blogposts.go
@@ -4,15 +4,19 @@ import (
 	"io/fs"
 )
 
-func PostFromFS(filesystem fs.FS) []Post {
-  dir, _ := fs.ReadDir(filesystem, ".")
+func PostFromFS(filesystem fs.FS) ([]Post, error) {
+  dir, err := fs.ReadDir(filesystem, ".")
+
+  if err != nil {
+    return nil, err
+  }
 
   var posts []Post
   for _, f := range dir {
     post := makePostFromFile(filesystem, f.Name())
     posts = append(posts, post)
   }
-  return posts
+  return posts, nil
 }
 
 func makePostFromFile(filesystem fs.FS, fileName string) Post {
--- blogposts_test.go
+++ blogposts_test.go
@@ -2,28 +2,48 @@ package blogposts_test
 
 import (
 	"blogposts"
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
 func TestBlogPosts(t *testing.T) {
-  // Given
-  fs := fstest.MapFS{
-    "hello-world.md": {Data: []byte("Title: hello, world!")},
-    "hello-world2.md": {Data: []byte("Title: Hola, mundo!")},
+  t.Run("happy path", func(t *testing.T) { 
+    // Given
+    fs := fstest.MapFS{
+      "hello-world.md": {Data: []byte("Title: hello, world!")},
+      "hello-world2.md": {Data: []byte("Title: Hola, mundo!")},
+    }
+    
+    // When
+    posts, err := blogposts.PostFromFS(fs)
+    
+    // Then
+    if err != nil {
+      t.Fatal(err)
+    }
+    
+    if len(posts)!= len(fs) {
+      t.Errorf("Expected %d posts, got %d", len(fs), len(posts))
   }
-
-  // When
-  posts := blogposts.PostFromFS(fs)
-
-  // Then
-  if len(posts)!= len(fs) {
-    t.Errorf("Expected %d posts, got %d", len(fs), len(posts))
-  }
-
+  
   want := blogposts.Post{Title:"hello, world!"}
 
   if posts[0] != want {
     t.Errorf("Expected %#v, got %#v", want, posts[0])
   }
+})
+t.Run("failing fs", func(t *testing.T) {
+  _, err := blogposts.PostFromFS(FailingFS{})
+  if err == nil {
+    t.Error("Expected an error, but didn't get one")
+  }
+})
+}
+type FailingFS struct {
+}
+func (f FailingFS) Open(name string) (fs.File, error) {
+	var err = errors.New("failed to open")
+	return nil, err
 }
